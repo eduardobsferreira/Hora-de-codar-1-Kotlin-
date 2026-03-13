6a75ddcb17cd8fe7c6db0542a9beeeef4409ccb2
Eduardo <eduardobarrossferreira@gmail.com>
2026-03-13 11:29:41 -0300

    Add files via upload

---
+++ nomedousuario/src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+     print("Informe o seu nome: ")
+    val NomeDousuário = readln()
+    println( "Olá, $NomeDousuário")
+    }
