5f918635efd7accd37e9c81cae05ba5ce50ec01f
Eduardo <eduardobarrossferreira@gmail.com>
2026-03-13 11:43:28 -0300

    Add files via upload

---
+++ nomeeidadedouser/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    print("Informe o seu nome: ")
+    val NomeDousuário = readln()
+    print("Informe sua idade:")
+    val idade = readln()
+    println( "Olá, $NomeDousuário! sua idade é: $idade")
+}
