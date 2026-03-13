067647cd00bc29054f4885fb61a92e40a27630ce
Eduardo <eduardobarrossferreira@gmail.com>
2026-03-13 10:54:28 -0300

    Add files via upload

---
+++ nomedofusca/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("\n--- INFORMAÇÕES DO CARRO---")
+    val nome_do_fusca = "Fusca"
+    println("Nome do carro:" + "${nome_do_fusca}")
+
+
+    }
