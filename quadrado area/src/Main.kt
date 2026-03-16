d59d246cfce5a4be8dbb4278f856932d15d61998
Eduardo <eduardobarrossferreira@gmail.com>
2026-03-16 11:18:46 -0300

    Add files via upload

---
+++ quadrado area/src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    println("--- CÁLCULO DA ÁREA DO QUADRADO ---")
+
+
+    println("Digite o lado:")
+    val lado = readln().toDouble()
+
+
+    val area = lado * lado
+    println ("O valor do cálculo da área do quadrado é: $area")
+
+}
